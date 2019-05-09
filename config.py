321d4fcf79bfd1f2d393e0a458ba901b3ff95464
Tran Tu Uyen <trantuuyenbg@gmail.com>
2019-05-09 21:00:04 +0700

    Update online DB

--- config.py
+++ config.py
@@ -2,6 +2,10 @@
 
 from pymongo import MongoClient
 
-DATABASE = MongoClient()['ABCBankDatabase'] # DB_NAME
-DEBUG = True
-client = MongoClient('localhost', 27017)
+# DATABASE = MongoClient()['ABCBankDatabase'] # DB_NAME
+# DEBUG = True
+# client = MongoClient('localhost', 27017)
+
+MONGODB_URI = "mongodb://trantuuyen:password1@ds153766.mlab.com:53766/abcbankportal"
+client = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
+
